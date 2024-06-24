20a0bb1ca86cd7469e7694fdd018fa827189e98a
SanchezsX -dev~ <113286578+SanchezsX@users.noreply.github.com>
2024-06-24 22:12:09 +0200

    add url in vite.config part 5

--- src/components/ButtonSearch.tsx
+++ src/components/ButtonSearch.tsx
@@ -2,7 +2,7 @@ const ButtonSearch = () => {
   return (
     <button className="w-[60px] h-[60px] bg-[#2972FF] flex flex-col items-center justify-center rounded-[8px]">
       <img
-        src="/icons/search.svg"
+        src="./icons/search.svg"
         alt="Search"
         className="size-[24px]"
       />
